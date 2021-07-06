188a86e5564a294a31db3e5110c1181db46aa329
Andre Viana <code.andre3@gmail.com>
2021-07-06 10:11:50 -0300

    first commit

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+
+
+
+@NgModule({
+  declarations: [
+    CalculadoraComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CalculadoraComponent
+  ]
+})
+export class CalculadoraModule { }
